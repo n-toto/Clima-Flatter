9b11fbf336ce0ef46a3c9282dc3668248d4dca3e
Naho <t.naho.1737@gmail.com>
2021-11-02 18:43:11 +0900

    Get current location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
+
+
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() async {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+      Location location = Location();
+      await location.getCurrentLocation();
+      print(location.latitude);
+      print(location.longitude);
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
+        // child: ElevatedButton(
+        //   onPressed: () {
+        //     getLocation();
+        //   },
+        //   child: Text('Get Location'),
+        // ),
       ),
     );
   }

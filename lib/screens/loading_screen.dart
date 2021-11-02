bc7b57c09dced11fe78bd9a74352516422ff0502
Naho <t.naho.1737@gmail.com>
2021-11-03 00:11:39 +0900

    Add web API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,38 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import 'package:clima/screens/location_screen.dart';
+
+const apiKey = '2a9f719af3380e91daafdb1b040467c5';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
-
-
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
 
   @override
-  void initState() async {
+  void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
-      Location location = Location();
-      await location.getCurrentLocation();
-      print(location.latitude);
-      print(location.longitude);
-    }
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherData = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
+    double temperature = weatherData['main']['temp'];
+    int condition = weatherData['weather'][0]['id'];
+    String cityName = weatherData['name'];
+
+    print(temperature);
+    print(condition);
+    print(cityName);
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        // child: ElevatedButton(
-        //   onPressed: () {
-        //     getLocation();
-        //   },
-        //   child: Text('Get Location'),
-        // ),
+        child: SpinKitDoubleBounce(
+          color:Colors.white,
+          size: 100,
+        ),
       ),
     );
   }
